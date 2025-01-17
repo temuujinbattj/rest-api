3e54da687ff180e7ce16844ab250a762e07a52f3
BE12 <be01@Erxess-iMac.local>
2025-01-17 21:08:52 +0800

    add middleware for api key

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
 import express from "express";
 import fs from "fs";
 import { route as movieRoutes } from "./modules/movies/routes/moviesRoutes.js";
+import { nanoid } from "nanoid";
+
+const verifyKey = (req, res, next) => {
+  const { moviekey } = req.headers;
+
+  const key = JSON.parse(fs.readFileSync("./key.txt"));
+
+  if (moviekey !== key) {
+    return res.send({ ok: false, message: "Invalid Key" });
+  }
+
+  next();
+};
 
 const app = express();
 
 app.use(express.json());
 
+app.use(verifyKey);
+
 app.use("/movies", movieRoutes);
+
+app.get("/keyRequest", (req, res) => {
+  const key = nanoid();
+
+  fs.writeFileSync("./key.txt", JSON.stringify(key));
+
+  res.send({ key });
+});
 
 app.get("/", (req, res) => {
   //html info page
